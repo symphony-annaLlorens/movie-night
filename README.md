e73de2fb7e7a611d575903c05b3494d4383080e2
Anna Llorens <anna.llorens@symphony.com>
2022-07-15 19:51:13 +0200

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# movie-night
+# MOVIE NIGHT
+
+This repo cointains a React app that implements a Frontend web site to search and give information about movies.
+
+## Getting started
+
+To get the frontend running locally:
+
+- Clone this repo
+- `yarn` to install all req'd dependencies
+- `yarn dev` to start the local server
+
+## Authors
+
+- **Anna Llorens Roig**
---
+++ pages/index.tsx
@@ -0,0 +1,29 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Movie Night</title>
+        <meta name="description" content="Streaming service to watch TV shows and movies" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main >
+        <h1>
+          MOVIENIGHT
+        </h1>
+
+   
+      </main>
+
+      <footer>
+      
+      </footer>
+    </div>
+  )
+}
+
+export default Home
